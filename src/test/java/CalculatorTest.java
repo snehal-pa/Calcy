df066c4ae7fdaf90246e2859d1906022fbcd1a56
snehal patel <snehal.kathiriya@gmail.com>
2020-11-17 21:56:59 +0100

    fail

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -14,7 +14,7 @@ class CalculatorTest {
     @Test
     void substraction() {
         var calc = new com.company.Calculator();
-        assertEquals(10,calc.substraction(10,3));
+        assertEquals(10,calc.substraction(13,3));
     }
 
 
