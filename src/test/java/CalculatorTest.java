2e676662e4746c9bb0ab99293c81226cc12cc07b
snehal patel <snehal.kathiriya@gmail.com>
2020-11-17 13:51:06 +0100

    test will pass

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,6 +8,6 @@ class CalculatorTest {
     @Test
     void addition() {
         var calc = new com.company.Calculator();
-        assertEquals(10,calc.addition(3,4));
+        assertEquals(7,calc.addition(3,4));
     }
 }
