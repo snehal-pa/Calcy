4a18247deddd42a1b0b013ec461adbb0b4cb6a53
snehal patel <snehal.kathiriya@gmail.com>
2020-11-17 09:48:49 +0100

    Initial commit

---
+++ src/main/java/com/company/Calculator.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Calculator {
+    public int addition(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void addition() {
+        var calc = new com.company.Calculator();
+        assertEquals(7,calc.addition(3,4));
+    }
+}
